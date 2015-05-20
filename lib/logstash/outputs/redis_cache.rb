7fcc3d68dc952bf6d1ab1d3f131ff4ebe13142ec
Adriano Paggi <adriano@pert.com.ar>
2015-05-20 12:55:50 -0300

    Prolijidad.

--- lib/logstash/outputs/redis_cache.rb
+++ lib/logstash/outputs/redis_cache.rb
@@ -1,21 +1,12 @@
 # encoding: utf-8
 require "logstash/outputs/base"
 require "logstash/namespace"
-require "stud/buffer"
 require "logstash/json"
 
-# This output will send events to a Redis queue using RPUSH.
-# The RPUSH command is supported in Redis v0.0.7+. Using
-# PUBLISH to a channel requires at least v1.3.8+.
-# While you may be able to make these Redis versions work,
-# the best performance and stability will be found in more 
-# recent stable versions.  Versions 2.6.0+ are recommended.
-#
-# For more information, see http://redis.io/[the Redis homepage]
-#
+# This output will cache some selected key-value pairs
+# to Redis using set command.
+# 
 class LogStash::Outputs::Redis_Cache < LogStash::Outputs::Base
-
-  include Stud::Buffer
 
   config_name "redis_cache"
 
@@ -35,7 +26,7 @@ class LogStash::Outputs::Redis_Cache < LogStash::Outputs::Base
   # The default port to connect on. Can be overridden on any hostname.
   config :port, :validate => :number, :default => 6379
 
-  # The Redis database number.
+  # The Redis database number. Defaults to 1 to differenciate from redis plugin which writes to "0 database" by default.
   config :db, :validate => :number, :default => 1
 
   # Redis initial connection timeout in seconds.
@@ -44,77 +35,22 @@ class LogStash::Outputs::Redis_Cache < LogStash::Outputs::Base
   # Password to authenticate with.  There is no authentication by default.
   config :password, :validate => :password
 
-  # The name of a field in event that will become in Redis key.
+  # The name of an existing field in event that will become in the Redis key.
   config :key, :validate => :string, :required => true
 
 	# List of field events that want to cache
 	config :fields, :validate => :array, :required => true
 
-  # Set to true if you want Redis to batch up values and send 1 RPUSH command
-  # instead of one command per value to push on the list.  Note that this only
-  # works with `data_type="list"` mode right now.
-  #
-  # If true, we send an RPUSH every "batch_events" events or
-  # "batch_timeout" seconds (whichever comes first).
-  # Only supported for `data_type` is "list".
-  config :batch, :validate => :boolean, :default => false
-
-  # If batch is set to true, the number of events we queue up for an RPUSH.
-  config :batch_events, :validate => :number, :default => 50
-
-  # If batch is set to true, the maximum amount of time between RPUSH commands
-  # when there are pending events to flush.
-  config :batch_timeout, :validate => :number, :default => 5
-
   # Interval for reconnecting to failed Redis connections
   config :reconnect_interval, :validate => :number, :default => 1
 
-  # In case Redis `data_type` is `list` and has more than `@congestion_threshold` items,
-  # block until someone consumes them and reduces congestion, otherwise if there are
-  # no consumers Redis will run out of memory, unless it was configured with OOM protection.
-  # But even with OOM protection, a single Redis list can block all other users of Redis,
-  # until Redis CPU consumption reaches the max allowed RAM size.
-  # A default value of 0 means that this limit is disabled.
-  # Only supported for `list` Redis `data_type`.
-  config :congestion_threshold, :validate => :number, :default => 0
-
-  # How often to check for congestion. Default is one second.
-  # Zero means to check on every event.
-  config :congestion_interval, :validate => :number, :default => 1
-
   def register
     require 'redis'
-
-#    if @batch
-    #  if @data_type != "list"
-    #    raise RuntimeError.new(
-    #      "batch is not supported with data_type #{@data_type}"
-    #    )
-    #  end
-#      buffer_initialize(
-#        :max_items => @batch_events,
-#        :max_interval => @batch_timeout,
-#        :logger => @logger
-#      )
-#    end
-
     @redis = nil
-    #if @shuffle_hosts
-    #    @host.shuffle!
-    #end
-    #@host_idx = 0
-
-    #@congestion_check_times = Hash.new { |h,k| h[k] = Time.now.to_i - @congestion_interval }
   end # def register
 
   def receive(event)
     return unless output?(event)
-
-#    if @batch and @data_type == 'list' # Don't use batched method for pubsub.
-#      # Stud::Buffer
-#      buffer_receive(event.to_json, event.sprintf(@key))
-#      return
-#    end
 
     key = event[@key]
     # TODO(sissel): We really should not drop an event, but historically
@@ -122,9 +58,7 @@ class LogStash::Outputs::Redis_Cache < LogStash::Outputs::Base
     # TODO(sissel): Find a way to continue passing events through even
     # if they fail to convert properly.
     begin
-      #payload = event.to_json
-			# devolvemos solo las claves que se mencionan en el array @fields (por ahora son de "primer orden")
-      #payload = event.to_hash.slice(@fields)
+			# Get only the keys listed in @fields (first order keys). Prepare the event with some filters to assure the existence of the required fields.
       payload = LogStash::Json.dump(event.to_hash.select { |k, v| @fields.include?(k) } )
     rescue Encoding::UndefinedConversionError, ArgumentError
       puts "FAILUREENCODING"
@@ -133,18 +67,9 @@ class LogStash::Outputs::Redis_Cache < LogStash::Outputs::Base
       return
     end
 
-		# Aca viene cuando no es BATCH
     begin
       @redis ||= connect
-#      if @data_type == 'list'
-#        congestion_check(key)
-#        @redis.rpush(key, payload)
-#      else
-#        @redis.publish(key, payload)
-#      end
-
-			 # El payload, es un string json-encoded, entonces eventualmente podemos hacer un mset, y mandar varios pares de clave-valoe
-			 @redis.set(key, payload)
+			@redis.set(key, payload)
 				
     rescue => e
       @logger.warn("Failed to send event to Redis", :event => event,
@@ -156,66 +81,8 @@ class LogStash::Outputs::Redis_Cache < LogStash::Outputs::Base
     end
   end # def receive
 
-
-###################################################### BUFFER #################################################################################################################
-#  def congestion_check(key)
-#    return if @congestion_threshold == 0
-#    if (Time.now.to_i - @congestion_check_times[key]) >= @congestion_interval # Check congestion only if enough time has passed since last check.
-#      while @redis.llen(key) > @congestion_threshold # Don't push event to Redis key which has reached @congestion_threshold.
-#        @logger.warn? and @logger.warn("Redis key size has hit a congestion threshold #{@congestion_threshold} suspending output for #{@congestion_interval} seconds")
-#        sleep @congestion_interval
-#      end
-#      @congestion_check_times[key] = Time.now.to_i
-#    end
-#  end
-
-
-	# Esto es solo para batch, no?
-	# El buffer asi nomas no sirve, porque "agrupa" los eventos.  A priori, cada evento es un string, con lo cual no podemos obtener la clave de aqui (si fueran hashes)
-	# Si queremos una version que bufferice, tenemos que laburarlo un poco.
-
-  # called from Stud::Buffer#buffer_flush when there are events to flush
-  def flush(events, key, teardown=false)
-    @redis ||= connect
-    # we should not block due to congestion on teardown
-    # to support this Stud::Buffer#buffer_flush should pass here the :final boolean value.
-#    congestion_check(key) unless teardown
-
-    #@redis.rpush(key, events)
-		# Si llegamos a usar el modo bufferizado, tendriamos que mandar n "eventos" a la vez
-    @redis.mset(key, events)
-  end
-  # called from Stud::Buffer#buffer_flush when an error occurs
-  def on_flush_error(e)
-    @logger.warn("Failed to send backlog of events to Redis",
-      :identity => identity,
-      :exception => e,
-      :backtrace => e.backtrace
-    )
-    @redis = connect
-  end
-
-  def teardown
-    if @batch
-      buffer_flush(:final => true)
-    end
-		# Adriano: ver que onda si no es ni channel ni list!
-    if @data_type == 'channel' and @redis
-      @redis.quit
-      @redis = nil
-    end
-  end
-################################################################################################################################################
-
   private
   def connect
-  #  @current_host, @current_port = @host[@host_idx].split(':')
-  #  @host_idx = @host_idx + 1 >= @host.length ? 0 : @host_idx + 1
-
-  #  if not @current_port
-  #    @current_port = @port
-  #  end
-
     params = {
       :host => @host,
       :port => @port,
--- spec/outputs/redis_cache_spec.rb
+++ spec/outputs/redis_cache_spec.rb
@@ -7,29 +7,26 @@ describe LogStash::Outputs::Redis_Cache, :redis_cache => true do
   
 
   describe "cache selected key-values from event in redis cache" do
-#    key = 10.times.collect { rand(10).to_s }.join("")
-		keys = ["192.168.1.1", "172.1.0.123"]
-#    event_count = 10000 + rand(500)
+    key = 10.times.collect { rand(10).to_s }.join("")
     event_count = 1
 
     config <<-CONFIG
       input {
         generator {
-          message => 'ip_SRC=192.168.1.1 usuario=adriano host=loco pirulo=filtrado'
+          message => 'clave=#{key} usuario=adriano host=loco pirulo=filtrado'
           count => #{event_count}
           type => "generator"
         }
       }
 			filter {
 				kv {
-					add_field => { "ip" => "%{ip_SRC}" }
 				}
 			}
       output {
         redis_cache {
           host => "127.0.0.1"
-          key => "ip_SRC"
-          fields => ["usuario", "host", "f1", "otro", "ip"]
+          key => "clave"
+          fields => ["usuario", "host", "f1", "otro"]
         }
       }
     CONFIG
@@ -39,99 +36,19 @@ describe LogStash::Outputs::Redis_Cache, :redis_cache => true do
       redis = Redis.new(:host => "127.0.0.1", :db => 1)
 
       # The list should contain the number of elements our agent pushed up.
-      insist { redis.dbsize } == [ event_count, keys.length ].min
+      insist { redis.dbsize } == event_count
 
       # Now check all events for order and correctness.
       event_count.times do |value|
-        ip = keys[value]
-				element = redis.get(ip)
+				element = redis.get(key)
         cached = LogStash::Event.new(LogStash::Json.load(element))
-        insist { cached["ip"] } == ip
+        insist { cached["host"] } == "loco"
         insist { cached["usuario"] } == "adriano"
-				redis.del( ip )
+				redis.del( key )
       end
 
       # The list should now be empty
       insist { redis.dbsize } == 0
     end # agent
   end
-
-#  describe "batch mode" do
-#    key = 10.times.collect { rand(10).to_s }.join("")
-#    event_count = 200000
-#
-#    config <<-CONFIG
-#      input {
-#        generator {
-#         message => "hello world"
-#          count => #{event_count}
-#          type => "generator"
-#        }
-#      }
-#      output {
-#        redis {
-#          host => "127.0.0.1"
-#          key => "#{key}"
-#          data_type => list
-#          batch => true
-#          batch_timeout => 5
-#          timeout => 5
-#        }
-#      }
-#    CONFIG
-#
-#    agent do
-#      # we have to wait for teardown to execute & flush the last batch.
-#      # otherwise we might start doing assertions before everything has been
-#      # sent out to redis.
-#      sleep 2
-#
-#      redis = Redis.new(:host => "127.0.0.1")
-#
-#      # The list should contain the number of elements our agent pushed up.
-#      insist { redis.llen(key) } == event_count
-#
-#      # Now check all events for order and correctness.
-#      event_count.times do |value|
-#        id, element = redis.blpop(key, 0)
-#        event = LogStash::Event.new(LogStash::Json.load(element))
-#        insist { event["sequence"] } == value
-#        insist { event["message"] } == "hello world"
-#      end
-#
-#      # The list should now be empty
-#      insist { redis.llen(key) } == 0
-#    end # agent
-#  end
-#
-#  describe "converts US-ASCII to utf-8 without failures" do
-#    key = 10.times.collect { rand(10).to_s }.join("")
-#
-#    config <<-CONFIG
-#      input {
-#        generator {
-#          charset => "US-ASCII"
-#          message => "\xAD\u0000"
-#          count => 1
-#          type => "generator"
-#        }
-#      }
-#      output {
-#        redis {
-#          host => "127.0.0.1"
-#          key => "#{key}"
-#          data_type => list
-#        }
-#      }
-#    CONFIG
-#
-#    agent do
-#      # Query redis directly and inspect the goodness.
-#      redis = Redis.new(:host => "127.0.0.1")
-#
-#      # The list should contain no elements.
-#      insist { redis.llen(key) } == 1
-#    end # agent
-#  end
 end
-
